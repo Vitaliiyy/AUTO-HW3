c7b157616263bd1f8df391532c47192304883dbd
Vitaliy <vetal.com92@gmail.com>
2023-08-17 17:12:17 +0600

    newPort

--- src/test/java/ru/netology/web/SelenideTest.java
+++ src/test/java/ru/netology/web/SelenideTest.java
@@ -27,7 +27,7 @@ class selenideTest {
                 options.addArguments("--no-sandbox");
                 options.addArguments("--headless");
                 driver = new ChromeDriver(options);
-                driver.get("http://localhost:7777");
+                driver.get("http://localhost:9999");
         }
 
         @AfterEach
